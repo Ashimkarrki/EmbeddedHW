debb27e49459dd31a66c11eb62125d81ad2ac54e
ashishwebpage <narruto543@gmail.com>
2024-02-20 21:38:03 +0545

    bug fixed

--- decoder2X4/decoder.vhdl
+++ decoder2X4/decoder.vhdl
@@ -15,8 +15,8 @@ begin
     dc: process(I)
     begin
 	O(0) <= not I(0) and not I(1);
-	O(1) <= not I(0) and I(1);
-	O(2) <= I(0) and not I(1);
+	O(1) <=  I(0) and not I(1);
+	O(2) <= not I(0) and  I(1);
 	O(3) <= I(0) and I(1);
     end process dc;
 end behavior; 
