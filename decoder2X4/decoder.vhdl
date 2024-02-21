a5df73abae66371fdf1d148d93d03a10dd613a43
Ashimkarrki <ashimkarki987@gmail.com>
2024-02-21 06:37:20 +0545

    bug fixed

--- decoder2X4/decoder.vhdl
+++ decoder2X4/decoder.vhdl
@@ -14,8 +14,8 @@ architecture behavior of decoder is
 begin
     dc: process(I)
     begin
-	O(0) <=not I(0) and not I(1);
-	O(1) <=  I(0) and not I(1);
+	O(0) <= not I(0) and not I(1);
+	O(1) <= not I(1) and  I(0);
 	O(2) <= not I(0) and  I(1);
 	O(3) <= I(1) and I(0);
     end process dc;
